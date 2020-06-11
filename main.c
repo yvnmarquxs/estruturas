60af3b1f19dfe6f376767715492b0f57a634be63
yanmarques <yanmarques@aluno.fapce.edu.br>
2020-06-11 19:44:40 +0000

    .

--- main.c
+++ main.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int ascending(void const *a, void const *b )
-{
-    return (*(int*)a - *(int*)b );
-}
 
-int descending(void const *a, void const *b )
-{
-    return (*(int*)b - *(int*)a );
-}
-
+int mdc(int a,int b);
 int main(){
-    int n, i, num, par, impar;
-    
-    scanf("%d", &n);
-    par = 0;
-    impar = 0;
-    int np[n];
-    int ni[n];
-    
-    for(i = 0; i < n; i++){
-        scanf("%d", &num);
-        if(num%2 == 0){
-            np[par] = num;
-            par++;
-        }else{
-            ni[impar] = num;
-            impar++;
-        }
-    }
-    qsort(np, par, sizeof(int), ascending);
-    qsort(ni, impar, sizeof(int), descending);
-    for(i = 0; i < par; i++){
-        printf("%d\n",np[i]);
-    }
-    for(i = 0; i < impar; i++){
-        printf("%d\n",ni[i]);
-    }
-    
-    return 0;
+ int n, x, n1, d1, n2, d2,num, mdcr, den;
+ char o;
+
+ scanf("%d\n",&n);
+ for(x = 0; x < n; x++){
+  scanf("%d / %d %c %d / %d",&n1,&d1,&o,&n2,&d2);
+  if(o == '/'){
+   num = d1*n2;
+   den = n1*d2;
+   mdcr = mdc(den,num);
+   printf("%d/%d = %d/%d\n",den,num,den/mdcr,num/mdcr);
+  }
+  else{
+   num = d1*d2;
+   if(o == '+')
+   den = n1*(num/d1) + n2*(num/d2);
+   else if(o == '-')
+   den = n1*(num/d1) - n2*(num/d2);
+   else if(o == '*')
+   den = n1*n2;
+   mdcr = mdc(den,num);
+   if(mdcr < 0) mdcr = -1*mdcr;
+   printf("%d/%d = %d/%d\n",den,num,den/mdcr,num/mdcr);
+  }
+ }
+ return 0;
 }
+
+int mdc(int a,int b)
+ {
+ if(b == 0) 
+  return a;
+ else
+  return mdc(b,a%b);
+} 
